d77dabe871cd52669fd01b75a39e0e9325287807
Murat Alibek <71397200+muratalibek@users.noreply.github.com>
2025-05-01 19:30:33 +0500

    Сделал задачу на линия 12. Программа выводит Hello World

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        /*Given an array of integers nums and an integer target, return indices of the two numbers such that they
+        add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order.*/
+        System.out.println("Hello World!");
+
+    }
+}
